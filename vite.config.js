37dd70a9928504eebfc8506eee27cb1dd3f184be
Samiran Ghosh <ranar.samiran@gmail.com>
2026-05-18 00:58:53 +0530

    first commit2

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5111',
+        target: 'http://101.53.134.89:5111',
         changeOrigin: true,
       },
       '/uploads': {
-        target: 'http://localhost:5111',
+        target: 'http://101.53.134.89:5111',
         changeOrigin: true,
       },
     },
